7867fbf9c89c73960ed8041ae69effa6313634e4
sk8erece1 <44900269+sk8erece1@users.noreply.github.com>
2018-11-22 21:35:33 +0300

    Add files via upload

---
+++ baslat.bat
@@ -0,0 +1,6 @@
+echo off
+color 0f
+cls
+:a
+node MusicBot.js
+goto a
---
+++ config.js
@@ -0,0 +1,5 @@
+exports.TOKEN = 'NTEzMDI5NTY4NTA5MTE2NDI3.Dthxyw.ufOsZB8lyaYaLSxuqcHDcE0evWg';
+
+exports.PREFIX = '/';
+
+exports.GOOGLE_API_KEY = 'AIzaSyAWVQ_0II-UF4VjulQVh6gAmdYURgmJQ_M';
